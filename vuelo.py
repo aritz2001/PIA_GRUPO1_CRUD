b06b5416c65672d30d07e11d812bbfa88a82cc17
davilobcicero <davilobcicero@gmail.com>
2023-09-29 11:00:55 +0200

    Insertado

--- vuelo.py
+++ vuelo.py
@@ -1,6 +1,3 @@
-import uuid
-import datetime
-
 class vuelo:
     def __init__(self):
         pass
@@ -10,13 +7,6 @@ class vuelo:
         self.destino = destino_json
         self.salida = salida_json
         self.plazas = plazas_json
-
-
-    def crear(self, destino:str, salida:datetime, plazas:int):
-        self.id = uuid.uuid1()
-        self.destino = destino
-        self.salida = salida.datetime.timespan()
-        self.plazas = plazas
         
     def __str__(self):
         return f"{self.id}\n{self.destino}\n{self.salida}\n{self.plazas} \n"            
